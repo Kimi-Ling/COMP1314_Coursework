db9cabe3254dab7a60dc84b6f1d8876214585743
Kimi-Ling <kwal1e24@soton.ac.uk>
2025-12-11 19:28:29 +0800

    Get the daily max and min gold and silver price

---
+++ max_min_silver_price_week1_daily.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the max and min price from each day
+awk '{
+
+        date = $1
+        price = $3
+
+        if (!(date in max) || price >  max[date]) max[date] = price
+        if (!(date in min) || price < min[date]) min[date] = price
+
+}
+
+END{
+        print "date max_price min_price"
+        for (d in max) print d, max[d], min[d]
+}' silver_week1_clean.txt | sort > max_min_week1_daily_silver.txt
+
